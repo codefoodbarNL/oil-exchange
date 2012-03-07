20c356c7caab0318fa276e35a00f99a9dbd03241
Albert Sikkema <albert.sikkema@uppenergy.com>
2012-03-07 21:44:56 +0100

    almost done (tempting stuff not working)

--- public/javascripts/modules/oilModule.js
+++ public/javascripts/modules/oilModule.js
@@ -7,17 +7,22 @@
     Oil.Collection = Backbone.Collection.extend({
         url: '/products',
 
-        model: Oil.Model
+        model: Oil.Model,
+
+        parse: function(response) {
+            return response.products;
+        }
     });
 
     Oil.Router = Backbone.Router.extend({
 
         initialize: function() {
             this.collection = new Oil.Collection();
+            this.collection.fetch();            
         },
 
         routes: {
-            "": "listProducts",
+            "": "menu",
             "productlist": "listProducts",
             "product/:productId": "productDetail"
         },
@@ -28,7 +33,6 @@
 
         listProducts: function() {
             console.log('list of products');
-
             this.view = new Oil.ProductsView({model: this.collection});
             $('#content').html(this.view.render().el);
         },
@@ -36,26 +40,37 @@
         productDetail: function(id) {
             var model = this.collection.get(id);
             this.view = new Oil.ProductView({model: model});
-            this.view.render();
+            $('#content').html(this.view.render().el);
         }
     });
 
     Oil.ProductsView = Backbone.View.extend({
 
         initialize: function() {
-            this.model.bind("change", this.render, this);
+            this.model.bind("reset", this.render, this);
         },
 
         render: function() {
             console.log('render products list');
-            $(this.el).html('products');
+            var names = _.map(this.models, function(m) {
+                return m.name;
+            });
+
+            var data = {
+                "data" : names
+            };
+
+            var t = '<p>Products:</p><br/><ul>{{#data}<li>{{name}}</li>{{/data}}</ul>';
+
+            $(this.el).html(Mustache.render(t, data));
             return this;
         }
     });
 
     Oil.ProductView = Backbone.View.extend({
         render: function() {
-            console.log('render product details for ' + this.model.id);
+            $(this.el).html('product detail');
+            console.log('render product details for ');
             return this;
         }
     });
